3c1fbbcb86a5eb887cb345f97664f0504ae9323e
PhilippeMartin1964 <philippe.martin@bluewin.ch>
2026-05-06 20:07:52 +0200

    Move editor mode registration to editor module

--- Source/GrimrockPrototype/GrimrockPrototype.cpp
+++ Source/GrimrockPrototype/GrimrockPrototype.cpp
@@ -1,37 +1,3 @@
 #include "Modules/ModuleManager.h"
 
-#if WITH_EDITOR
-#include "EditorModeRegistry.h"
-#include "EditorTools/GridLevelEdMode.h"
-#endif
-
-class FGrimrockPrototypeModule : public FDefaultGameModuleImpl
-{
-public:
-    virtual void StartupModule () override
-    {
-        FDefaultGameModuleImpl::StartupModule ();
-
-#if WITH_EDITOR
-        FEditorModeRegistry::Get ().RegisterMode<FGridLevelEdMode> (
-            FGridLevelEdMode::EM_GridLevelEdModeId,
-            FText::FromString (TEXT ("Grimrock Grid Editor")),
-            FSlateIcon (),
-            true);
-#endif
-    }
-
-    virtual void ShutdownModule () override
-    {
-#if WITH_EDITOR
-        if (FModuleManager::Get ().IsModuleLoaded ("UnrealEd"))
-        {
-            FEditorModeRegistry::Get ().UnregisterMode (FGridLevelEdMode::EM_GridLevelEdModeId);
-        }
-#endif
-
-        FDefaultGameModuleImpl::ShutdownModule ();
-    }
-};
-
-IMPLEMENT_PRIMARY_GAME_MODULE (FGrimrockPrototypeModule, GrimrockPrototype, "GrimrockPrototype");
+IMPLEMENT_PRIMARY_GAME_MODULE (FDefaultGameModuleImpl, GrimrockPrototype, "GrimrockPrototype");
--- Source/GrimrockPrototypeEditor/GrimrockPrototypeEditor.cpp
+++ Source/GrimrockPrototypeEditor/GrimrockPrototypeEditor.cpp
@@ -1,14 +1,25 @@
 #include "Modules/ModuleManager.h"
+#include "EditorModeRegistry.h"
+#include "EditorTools/GridLevelEdMode.h"
 
 class FGrimrockPrototypeEditorModule : public IModuleInterface
 {
 public:
     virtual void StartupModule() override
     {
+        FEditorModeRegistry::Get().RegisterMode<FGridLevelEdMode>(
+            FGridLevelEdMode::EM_GridLevelEdModeId,
+            FText::FromString(TEXT("Grimrock Grid Editor")),
+            FSlateIcon(),
+            true);
     }
 
     virtual void ShutdownModule() override
     {
+        if (FModuleManager::Get().IsModuleLoaded("UnrealEd"))
+        {
+            FEditorModeRegistry::Get().UnregisterMode(FGridLevelEdMode::EM_GridLevelEdModeId);
+        }
     }
 };
 
