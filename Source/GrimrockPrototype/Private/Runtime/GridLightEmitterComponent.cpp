d554e823879a18a98cc22c416ede13a8aac1ada2
PhilippeMartin1964 <philippe.martin@bluewin.ch>
2026-05-07 15:10:51 +0200

    Decouple item light emitter offsets

--- Source/GrimrockPrototype/Private/Runtime/GridLightEmitterComponent.cpp
+++ Source/GrimrockPrototype/Private/Runtime/GridLightEmitterComponent.cpp
@@ -28,11 +28,13 @@ void UGridLightEmitterComponent::SetLightEnabled (bool bEnabled)
             NiagaraComponent->SetupAttachment (this);
             NiagaraComponent->SetAsset (NiagaraSystem);
             NiagaraComponent->RegisterComponent ();
+            RefreshEmitterTransforms ();
         }
     }
 
     if (NiagaraComponent)
     {
+        RefreshEmitterTransforms ();
         NiagaraComponent->SetVisibility (bEnabled, true);
         if (bEnabled)
         {
@@ -52,15 +54,32 @@ void UGridLightEmitterComponent::SetLightEnabled (bool bEnabled)
             PointLightComponent->bUseInverseSquaredFalloff = false;
             PointLightComponent->LightFalloffExponent = 4.f;
             PointLightComponent->RegisterComponent ();
+            RefreshEmitterTransforms ();
         }
     }
 
     if (PointLightComponent)
     {
+        RefreshEmitterTransforms ();
         PointLightComponent->SetIntensity (LightIntensity);
         PointLightComponent->SetAttenuationRadius (LightRadius);
         PointLightComponent->SetLightColor (LightColor);
         PointLightComponent->SetVisibility (bEnabled && bUsePointLight);
+    }
+}
+
+void UGridLightEmitterComponent::RefreshEmitterTransforms ()
+{
+    if (NiagaraComponent)
+    {
+        NiagaraComponent->SetRelativeLocation (NiagaraRelativeLocation);
+        NiagaraComponent->SetRelativeRotation (NiagaraRelativeRotation);
+    }
+
+    if (PointLightComponent)
+    {
+        PointLightComponent->SetRelativeLocation (PointLightRelativeLocation);
+        PointLightComponent->SetRelativeRotation (PointLightRelativeRotation);
     }
 }
 
--- Source/GrimrockPrototype/Public/Runtime/GridLightEmitterComponent.h
+++ Source/GrimrockPrototype/Public/Runtime/GridLightEmitterComponent.h
@@ -19,23 +19,38 @@ public:
     UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light")
     TObjectPtr<UNiagaraSystem> NiagaraSystem;
 
+    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light|Niagara")
+    FVector NiagaraRelativeLocation = FVector::ZeroVector;
+
+    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light|Niagara")
+    FRotator NiagaraRelativeRotation = FRotator::ZeroRotator;
+
     UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light")
     bool bEnableOnBeginPlay = true;
 
     UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light")
     bool bUsePointLight = true;
 
-    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light", meta = (EditCondition = "bUsePointLight", ClampMin = "0.0"))
+    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light|Point Light", meta = (EditCondition = "bUsePointLight"))
+    FVector PointLightRelativeLocation = FVector::ZeroVector;
+
+    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light|Point Light", meta = (EditCondition = "bUsePointLight"))
+    FRotator PointLightRelativeRotation = FRotator::ZeroRotator;
+
+    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light|Point Light", meta = (EditCondition = "bUsePointLight", ClampMin = "0.0"))
     float LightIntensity = 850.f;
 
-    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light", meta = (EditCondition = "bUsePointLight", ClampMin = "0.0"))
+    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light|Point Light", meta = (EditCondition = "bUsePointLight", ClampMin = "0.0"))
     float LightRadius = 325.f;
 
-    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light", meta = (EditCondition = "bUsePointLight"))
+    UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Light|Point Light", meta = (EditCondition = "bUsePointLight"))
     FLinearColor LightColor = FLinearColor (1.f, 0.55f, 0.22f, 1.f);
 
     UFUNCTION (BlueprintCallable, Category = "Light")
     void SetLightEnabled (bool bEnabled);
+
+    UFUNCTION (BlueprintCallable, Category = "Light")
+    void RefreshEmitterTransforms ();
 
     UFUNCTION (BlueprintCallable, Category = "Light")
     bool IsLightEnabled () const;
