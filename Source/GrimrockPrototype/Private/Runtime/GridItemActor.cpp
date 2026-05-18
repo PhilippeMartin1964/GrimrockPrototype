ac578e9455bc58f545004aa2a008a83dd92565a0
PhilippeMartin1964 <philippe.martin@bluewin.ch>
2026-05-18 16:48:16 +0200

    Initialize item lights from archetypes

--- Source/GrimrockPrototype/Private/Runtime/GridItemActor.cpp
+++ Source/GrimrockPrototype/Private/Runtime/GridItemActor.cpp
@@ -1,7 +1,9 @@
 #include "Runtime/GridItemActor.h"
 
+#include "Components/PointLightComponent.h"
 #include "Components/SceneComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Core/GridObjectArchetypeAsset.h"
 #include "Runtime/GridLightEmitterComponent.h"
 
 AGridItemActor::AGridItemActor ()
@@ -14,6 +16,12 @@ AGridItemActor::AGridItemActor ()
     MeshComponent = CreateDefaultSubobject<UStaticMeshComponent> (TEXT ("Mesh"));
     MeshComponent->SetupAttachment (SceneRoot);
     MeshComponent->SetCollisionEnabled (ECollisionEnabled::NoCollision);
+
+    ItemLightComponent = CreateDefaultSubobject<UPointLightComponent> (TEXT ("ItemLight"));
+    ItemLightComponent->SetupAttachment (SceneRoot);
+    ItemLightComponent->SetVisibility (false);
+    ItemLightComponent->SetIntensity (0.f);
+    ItemLightComponent->SetAttenuationRadius (250.f);
 }
 
 void AGridItemActor::InitializeItem (FName InArchetypeId, const TArray<FName>& InItemTags, UStaticMesh* Mesh, UMaterialInterface* Material)
@@ -31,21 +39,80 @@ void AGridItemActor::InitializeItem (FName InArchetypeId, const TArray<FName>& I
     }
 }
 
-void AGridItemActor::OnPlacedInWorld ()
+void AGridItemActor::InitializeItemFromArchetype (const UGridObjectArchetypeAsset* ItemArchetype)
 {
+    if (!ItemArchetype)
+    {
+        InitializeItem (NAME_None, TArray<FName> (), nullptr, nullptr);
+        ApplyItemLightFromArchetype (nullptr);
+        return;
+    }
+
+    UStaticMesh* ItemMesh = ItemArchetype->MovingMesh ? ItemArchetype->MovingMesh.Get () : ItemArchetype->PreviewMesh.Get ();
+    if (!ItemMesh)
+    {
+        ItemMesh = ItemArchetype->FixedMesh.Get ();
+    }
+
+    UMaterialInterface* ItemMaterial = ItemArchetype->MovingMaterial ? ItemArchetype->MovingMaterial.Get () : ItemArchetype->PreviewMaterial.Get ();
+    if (!ItemMaterial)
+    {
+        ItemMaterial = ItemArchetype->FixedMaterial.Get ();
+    }
+
+    InitializeItem (ItemArchetype->ArchetypeId, ItemArchetype->ItemTags, ItemMesh, ItemMaterial);
+    ApplyItemLightFromArchetype (ItemArchetype);
+}
+
+void AGridItemActor::ApplyItemLightFromArchetype (const UGridObjectArchetypeAsset* ItemArchetype)
+{
+    bHasArchetypeLightConfig = true;
+    bRuntimeIsLightSource = ItemArchetype && ItemArchetype->bIsLightSource;
+
     TArray<UGridLightEmitterComponent*> LightEmitters;
     GetComponents<UGridLightEmitterComponent> (LightEmitters);
 
     for (UGridLightEmitterComponent* LightEmitter : LightEmitters)
     {
-        if (LightEmitter)
+        if (!LightEmitter)
         {
-            LightEmitter->SetLightEnabled (true);
+            continue;
+        }
+
+        if (ItemArchetype)
+        {
+            LightEmitter->LightColor = ItemArchetype->LightColor;
+            LightEmitter->LightIntensity = ItemArchetype->LightIntensity;
+            LightEmitter->LightRadius = ItemArchetype->LightRadius;
+            LightEmitter->BaseLightColor = ItemArchetype->LightColor;
+            LightEmitter->BaseLightIntensity = ItemArchetype->LightIntensity;
+            LightEmitter->BaseAttenuationRadius = ItemArchetype->LightRadius;
+            LightEmitter->bEnableLightFlicker = ItemArchetype->bUseLightFlicker;
+            LightEmitter->bEnableLightColorFlicker = ItemArchetype->bUseLightFlicker;
+            LightEmitter->bEnableLightPositionFlicker = ItemArchetype->bUseLightFlicker;
+        }
+
+        LightEmitter->SetLightEnabled (bRuntimeIsLightSource);
+    }
+
+    const bool bUseNativeLightFallback = bRuntimeIsLightSource && LightEmitters.Num () == 0;
+    if (ItemLightComponent)
+    {
+        ItemLightComponent->SetVisibility (bUseNativeLightFallback);
+        if (bUseNativeLightFallback && ItemArchetype)
+        {
+            ItemLightComponent->SetLightColor (ItemArchetype->LightColor);
+            ItemLightComponent->SetIntensity (ItemArchetype->LightIntensity);
+            ItemLightComponent->SetAttenuationRadius (ItemArchetype->LightRadius);
+        }
+        else
+        {
+            ItemLightComponent->SetIntensity (0.f);
         }
     }
 }
 
-void AGridItemActor::OnRemovedFromWorld ()
+void AGridItemActor::SetRuntimeLightEnabled (bool bEnabled)
 {
     TArray<UGridLightEmitterComponent*> LightEmitters;
     GetComponents<UGridLightEmitterComponent> (LightEmitters);
@@ -54,9 +121,25 @@ void AGridItemActor::OnRemovedFromWorld ()
     {
         if (LightEmitter)
         {
-            LightEmitter->SetLightEnabled (false);
+            LightEmitter->SetLightEnabled (bEnabled);
         }
     }
+
+    if (ItemLightComponent)
+    {
+        const bool bUseNativeLight = bEnabled && bHasArchetypeLightConfig && LightEmitters.Num () == 0;
+        ItemLightComponent->SetVisibility (bUseNativeLight);
+    }
+}
+
+void AGridItemActor::OnPlacedInWorld ()
+{
+    SetRuntimeLightEnabled (bHasArchetypeLightConfig ? bRuntimeIsLightSource : true);
+}
+
+void AGridItemActor::OnRemovedFromWorld ()
+{
+    SetRuntimeLightEnabled (false);
 }
 
 bool AGridItemActor::HasItemTag (FName Tag) const
--- Source/GrimrockPrototype/Public/Runtime/GridItemActor.h
+++ Source/GrimrockPrototype/Public/Runtime/GridItemActor.h
@@ -5,6 +5,8 @@
 #include "GridItemActor.generated.h"
 
 class UStaticMeshComponent;
+class UGridObjectArchetypeAsset;
+class UPointLightComponent;
 
 UCLASS (Blueprintable)
 class GRIMROCKPROTOTYPE_API AGridItemActor : public AActor
@@ -20,6 +22,9 @@ public:
     UPROPERTY (VisibleAnywhere, BlueprintReadOnly, Category = "Components")
     TObjectPtr<UStaticMeshComponent> MeshComponent;
 
+    UPROPERTY (VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    TObjectPtr<UPointLightComponent> ItemLightComponent;
+
     UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = "Item")
     FName ArchetypeId = NAME_None;
 
@@ -30,6 +35,9 @@ public:
     virtual void InitializeItem (FName InArchetypeId, const TArray<FName>& InItemTags, UStaticMesh* Mesh, UMaterialInterface* Material);
 
     UFUNCTION (BlueprintCallable, Category = "Item")
+    virtual void InitializeItemFromArchetype (const UGridObjectArchetypeAsset* ItemArchetype);
+
+    UFUNCTION (BlueprintCallable, Category = "Item")
     virtual void OnPlacedInWorld ();
 
     UFUNCTION (BlueprintCallable, Category = "Item")
@@ -37,4 +45,11 @@ public:
 
     UFUNCTION (BlueprintCallable, Category = "Item")
     bool HasItemTag (FName Tag) const;
+
+protected:
+    bool bHasArchetypeLightConfig = false;
+    bool bRuntimeIsLightSource = false;
+
+    void ApplyItemLightFromArchetype (const UGridObjectArchetypeAsset* ItemArchetype);
+    void SetRuntimeLightEnabled (bool bEnabled);
 };
