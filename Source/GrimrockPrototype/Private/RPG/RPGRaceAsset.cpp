8d0011404742084ead8513efd9e1f24afe8ec4bc
Philippe Martin <129649670+PhilippeMartin1964@users.noreply.github.com>
2026-06-22 10:47:54 +0200

    Harden RPGRaceAsset.cpp initialization

--- Source/GrimrockPrototype/Private/RPG/RPGRaceAsset.cpp
+++ Source/GrimrockPrototype/Private/RPG/RPGRaceAsset.cpp
@@ -1,5 +1,10 @@
 #include "RPG/RPGRaceAsset.h"
 
+URPGRaceAsset::URPGRaceAsset ()
+    : AttributeBonuses (0, 0, 0, 0, 0, 0)
+{
+}
+
 bool URPGRaceAsset::IsValidDefinition () const
 {
     return !RaceId.IsNone ();
