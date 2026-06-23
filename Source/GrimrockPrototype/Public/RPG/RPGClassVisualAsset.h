8554690ab4f97a7bca08509ecaf72c0d82bbd1a5
Philippe Martin <129649670+PhilippeMartin1964@users.noreply.github.com>
2026-06-23 21:28:16 +0200

    Add CC6.4 class visual asset header

---
+++ Source/GrimrockPrototype/Public/RPG/RPGClassVisualAsset.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "RPGClassVisualAsset.generated.h"
+
+class UTexture2D;
+
+UCLASS (BlueprintType)
+class GRIMROCKPROTOTYPE_API URPGClassVisualAsset : public UPrimaryDataAsset
+{
+    GENERATED_BODY ()
+
+public:
+    UPROPERTY (EditAnywhere, BlueprintReadOnly, Category = "RPG|Class Visual")
+    FName ClassId = NAME_None;
+
+    UPROPERTY (EditAnywhere, BlueprintReadOnly, Category = "RPG|Class Visual")
+    FText DisplayName;
+
+    UPROPERTY (EditAnywhere, BlueprintReadOnly, Category = "RPG|Class Visual", meta = (MultiLine = "true"))
+    FText Description;
+
+    UPROPERTY (EditAnywhere, BlueprintReadOnly, Category = "RPG|Class Visual")
+    TSoftObjectPtr<UTexture2D> ClassIcon;
+
+    UPROPERTY (EditAnywhere, BlueprintReadOnly, Category = "RPG|Class Visual")
+    FLinearColor AccentColor = FLinearColor::White;
+
+    UFUNCTION (BlueprintPure, Category = "RPG|Class Visual")
+    bool IsValidDefinition () const;
+
+    UFUNCTION (BlueprintPure, Category = "RPG|Class Visual")
+    bool IsValidForClass (FName InClassId) const;
+};
