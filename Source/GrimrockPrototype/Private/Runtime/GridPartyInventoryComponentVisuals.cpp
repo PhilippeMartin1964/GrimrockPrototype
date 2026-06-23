101f7c1d9afbcdc5f1b142172ff782cb517c505f
Philippe Martin <129649670+PhilippeMartin1964@users.noreply.github.com>
2026-06-23 21:41:58 +0200

    Expose character visual selection after creation

--- Source/GrimrockPrototype/Private/Runtime/GridPartyInventoryComponentVisuals.cpp
+++ Source/GrimrockPrototype/Private/Runtime/GridPartyInventoryComponentVisuals.cpp
@@ -19,3 +19,23 @@ bool UGridPartyInventoryComponent::SetCharacterVisualSelection (
     CharacterState.ClassIcon = ClassIcon;
     return true;
 }
+
+bool UGridPartyInventoryComponent::GetCharacterVisualSelection (
+    int32 CharacterIndex,
+    FRPGCharacterVisualSelection& OutSelection) const
+{
+    OutSelection = FRPGCharacterVisualSelection ();
+    if (!IsValidCharacterIndex (CharacterIndex))
+    {
+        return false;
+    }
+
+    const FGridCharacterInventoryState& CharacterState = PartyInventoryState.ActiveCharacters[CharacterIndex];
+    OutSelection.RaceId = CharacterState.RaceId;
+    OutSelection.Gender = CharacterState.PortraitGender;
+    OutSelection.PortraitVariantId = CharacterState.PortraitVariantId;
+    OutSelection.Portrait = CharacterState.Portrait;
+    OutSelection.ClassId = CharacterState.ClassId;
+    OutSelection.ClassIcon = CharacterState.ClassIcon;
+    return true;
+}
