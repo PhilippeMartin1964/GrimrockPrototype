8d116f2a4ce7d5a56637cacb14015d9db3d12b0c
Philippe Martin <129649670+PhilippeMartin1964@users.noreply.github.com>
2026-07-02 16:11:59 +0200

    Remove inline attribute allocation fallback from wizard header

--- Source/GrimrockPrototype/Public/UI/RPGCharacterCreationWizardWidget.h
+++ Source/GrimrockPrototype/Public/UI/RPGCharacterCreationWizardWidget.h
@@ -89,11 +89,7 @@ public:
     virtual bool GetPreviewAttributes (FRPGAttributes& OutAttributes) const override;
     virtual bool GetPreviewDerivedStats (FRPGDerivedStats& OutDerivedStats) const override;
     virtual float GetPreviewCarryWeight () const override;
-
-    virtual bool SubmitCharacterCreation () override
-    {
-        return SubmitWizardCharacterCreation ();
-    }
+    virtual bool SubmitCharacterCreation () override;
 
     UFUNCTION (BlueprintCallable, Category = "RPG|Character Creation|Attributes")
     void ResetAttributeAllocationToClassDefinition ();
@@ -130,16 +126,12 @@ protected:
 
 private:
     void BindWizardButtons ();
-    void BindAttributeAllocationButtons ();
     void BindWizardSubmitButton ();
     void RefreshWizardShell ();
     void ApplyWizardStepToSwitcher ();
     UWidget* GetPanelForWizardStep (ERPGCharacterCreationWizardStep Step) const;
-    void RefreshAttributeAllocationPreview ();
-    void RefreshAttributeAllocationControls ();
     void EnsureAttributeAllocationInitialized ();
     TSoftObjectPtr<UTexture2D> ResolveWizardSelectedClassIcon () const;
-    bool SubmitWizardCharacterCreation ();
 
     UFUNCTION ()
     void HandlePreviousClicked ();
@@ -152,45 +144,6 @@ private:
 
     UFUNCTION ()
     void HandleWizardCreateCharacterClicked ();
-
-    UFUNCTION ()
-    void HandleResetRecommendedAttributesClicked ();
-
-    UFUNCTION ()
-    void HandleStrengthMinusClicked ();
-
-    UFUNCTION ()
-    void HandleStrengthPlusClicked ();
-
-    UFUNCTION ()
-    void HandleDexterityMinusClicked ();
-
-    UFUNCTION ()
-    void HandleDexterityPlusClicked ();
-
-    UFUNCTION ()
-    void HandleConstitutionMinusClicked ();
-
-    UFUNCTION ()
-    void HandleConstitutionPlusClicked ();
-
-    UFUNCTION ()
-    void HandleIntelligenceMinusClicked ();
-
-    UFUNCTION ()
-    void HandleIntelligencePlusClicked ();
-
-    UFUNCTION ()
-    void HandleWisdomMinusClicked ();
-
-    UFUNCTION ()
-    void HandleWisdomPlusClicked ();
-
-    UFUNCTION ()
-    void HandleCharismaMinusClicked ();
-
-    UFUNCTION ()
-    void HandleCharismaPlusClicked ();
 
 private:
     UPROPERTY (meta = (BindWidgetOptional))
@@ -228,141 +181,6 @@ private:
 
     UPROPERTY (meta = (BindWidgetOptional))
     TObjectPtr<UWidget> Panel_StepSummary;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_AttributePointsRemaining;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_AttributeHelp;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_ResetRecommendedAttributes;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_StrengthClassValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_StrengthRaceBonus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_StrengthTotalValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_StrengthModifier;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_StrengthEffects;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_StrengthMinus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_StrengthPlus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_DexterityClassValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_DexterityRaceBonus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_DexterityTotalValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_DexterityModifier;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_DexterityEffects;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_DexterityMinus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_DexterityPlus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_ConstitutionClassValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_ConstitutionRaceBonus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_ConstitutionTotalValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_ConstitutionModifier;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_ConstitutionEffects;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_ConstitutionMinus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_ConstitutionPlus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_IntelligenceClassValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_IntelligenceRaceBonus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_IntelligenceTotalValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_IntelligenceModifier;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_IntelligenceEffects;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_IntelligenceMinus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_IntelligencePlus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_WisdomClassValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_WisdomRaceBonus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_WisdomTotalValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_WisdomModifier;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_WisdomEffects;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_WisdomMinus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_WisdomPlus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_CharismaClassValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_CharismaRaceBonus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_CharismaTotalValue;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_CharismaModifier;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UTextBlock> Text_CharismaEffects;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_CharismaMinus;
-
-    UPROPERTY (meta = (BindWidgetOptional))
-    TObjectPtr<UButton> Button_CharismaPlus;
 
     UPROPERTY (Transient)
     FRPGAttributes AllocatedClassAttributes;
