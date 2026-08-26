e976f957677fef04456555f2186aebb8c0da489d
Philippe Martin <129649670+PhilippeMartin1964@users.noreply.github.com>
2026-08-26 08:31:47 +0200

    Extract TD02.4 party input buffer

---
+++ Source/GrimrockPrototype/Private/Runtime/GrimrockPartyPawnInputBuffer.cpp
@@ -0,0 +1,83 @@
+#include "Runtime/GrimrockPartyPawn.h"
+
+void AGrimrockPartyPawn::BufferMoveCommand(EGridEdge MoveDirection)
+{
+	if (!bEnableInputBuffer)
+	{
+		return;
+	}
+
+	BufferedCommandType = EBufferedCommandType::Move;
+	BufferedMoveDirection = MoveDirection;
+	bBufferedTurnRight = false;
+	BufferedCommandAge = 0.f;
+}
+
+void AGrimrockPartyPawn::BufferTurnCommand(bool bTurnRight)
+{
+	if (!bEnableInputBuffer)
+	{
+		return;
+	}
+
+	BufferedCommandType = EBufferedCommandType::Turn;
+	BufferedMoveDirection = EGridEdge::None;
+	bBufferedTurnRight = bTurnRight;
+	BufferedCommandAge = 0.f;
+}
+
+void AGrimrockPartyPawn::BufferUseCommand()
+{
+	if (!bEnableInputBuffer)
+	{
+		return;
+	}
+
+	BufferedCommandType = EBufferedCommandType::Use;
+	BufferedMoveDirection = EGridEdge::None;
+	bBufferedTurnRight = false;
+	BufferedCommandAge = 0.f;
+}
+
+void AGrimrockPartyPawn::ClearBufferedCommand()
+{
+	BufferedCommandType = EBufferedCommandType::None;
+	BufferedMoveDirection = EGridEdge::None;
+	bBufferedTurnRight = false;
+	BufferedCommandAge = 0.f;
+}
+
+bool AGrimrockPartyPawn::TryConsumeBufferedCommand()
+{
+	if (BufferedCommandType == EBufferedCommandType::None)
+	{
+		return false;
+	}
+
+	const EBufferedCommandType CommandType = BufferedCommandType;
+	const EGridEdge MoveDirection = BufferedMoveDirection;
+	const bool bTurnRight = bBufferedTurnRight;
+
+	ClearBufferedCommand();
+
+	switch (CommandType)
+	{
+		case EBufferedCommandType::Move:
+			return TryStartMove(MoveDirection);
+
+		case EBufferedCommandType::Turn:
+			return TryStartTurn(bTurnRight);
+
+		case EBufferedCommandType::Use:
+			return TryUseFrontInteraction();
+
+		default:
+			return false;
+	}
+}
+
+bool AGrimrockPartyPawn::IsBusy() const
+{
+	return bIsMoving || bIsTurning;
+}
+
