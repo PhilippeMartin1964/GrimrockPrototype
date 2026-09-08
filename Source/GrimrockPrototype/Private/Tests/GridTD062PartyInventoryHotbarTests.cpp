8e712f792361bdce46137a496702da73ab2edd0a
Philippe Martin <129649670+PhilippeMartin1964@users.noreply.github.com>
2026-09-08 16:24:50 +0200

    WORLDOBJ-MIG09 fix TD06.2 explicit item definition include

--- Source/GrimrockPrototype/Private/Tests/GridTD062PartyInventoryHotbarTests.cpp
+++ Source/GrimrockPrototype/Private/Tests/GridTD062PartyInventoryHotbarTests.cpp
@@ -1,6 +1,7 @@
 #if WITH_DEV_AUTOMATION_TESTS
 
 #include "Misc/AutomationTest.h"
+#include "Runtime/GridItemDefinitionAsset.h"
 #include "Runtime/GridPartyInventoryComponent.h"
 #include "UObject/Class.h"
 #include "UObject/UObjectGlobals.h"
